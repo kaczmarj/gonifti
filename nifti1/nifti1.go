b9b177cb91e08c19fe0ca200aaa4ebb1d688b0f5
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-03-27 17:58:17 -0400

    add logging + initial data read

--- nifti1/nifti1.go
+++ nifti1/nifti1.go
@@ -15,7 +15,12 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
+
+// Create logger
+var log = logrus.New()
 
 // Header defines the structure of the Nifti1 header.
 //
@@ -107,49 +112,49 @@ type mat33 struct {
 
 // Image is a high level image storage struct.
 type Image struct {
-	NDim     int16    // last dimension greater than 1 (1..7)
-	Nx       int16    // dimensions of grid array
-	Ny       int16    // dimensions of grid array
-	Nz       int16    // dimensions of grid array
-	Nt       int16    // dimensions of grid array
-	Nu       int16    // dimensions of grid array
-	Nv       int16    // dimensions of grid array
-	Nw       int16    // dimensions of grid array
-	Dim      [8]int16 // dim[0] = ndim, dim[1] = nx, etc
-	NVox     int32    // number of voxels = nx*ny*nz*...*nw
-	NByPer   int32    // bytes per voxel, matches datatype
-	DataType int32    // type of data in voxels: DT_* code
+	NDim     int    // last dimension greater than 1 (1..7)
+	Nx       int    // dimensions of grid array
+	Ny       int    // dimensions of grid array
+	Nz       int    // dimensions of grid array
+	Nt       int    // dimensions of grid array
+	Nu       int    // dimensions of grid array
+	Nv       int    // dimensions of grid array
+	Nw       int    // dimensions of grid array
+	Dim      [8]int // dim[0] = ndim, dim[1] = nx, etc
+	NVox     int    // number of voxels = nx*ny*nz*...*nw
+	NByPer   int    // bytes per voxel, matches datatype
+	DataType int    // type of data in voxels: DT_* code
 
-	Dx     float32    // grid spacings
-	Dy     float32    // grid spacings
-	Dz     float32    // grid spacings
-	Dt     float32    // grid spacings
-	Du     float32    // grid spacings
-	Dv     float32    // grid spacings
-	Dw     float32    // grid spacings
-	PixDim [8]float32 // pixdim[1]=dx, etc
+	Dx     float64    // grid spacings
+	Dy     float64    // grid spacings
+	Dz     float64    // grid spacings
+	Dt     float64    // grid spacings
+	Du     float64    // grid spacings
+	Dv     float64    // grid spacings
+	Dw     float64    // grid spacings
+	PixDim [8]float64 // pixdim[1]=dx, etc
 
-	SclSlope float32 // scaling parameter: slope
-	SclInter float32 // scaling parameter: intercept
+	SclSlope float64 // scaling parameter: slope
+	SclInter float64 // scaling parameter: intercept
 
-	CalMin float32 // calibration parameter: minimum
-	CalMax float32 // calbiration parameter: maximum
+	CalMin float64 // calibration parameter: minimum
+	CalMax float64 // calbiration parameter: maximum
 
-	QFormCode int32 // codes for (x,y,z) space meaning
-	SFormCode int32 // codes for (x,y,z) space meaning
+	QFormCode int // codes for (x,y,z) space meaning
+	SFormCode int // codes for (x,y,z) space meaning
 
-	FreqDim  int32 // indeces (1,2,3, or 0) for MRI
-	PhaseDim int32 // directions in dim[]/pixdim[]
-	SliceDim int32 // directions in dim[]/pixdim[]
+	FreqDim  int // indeces (1,2,3, or 0) for MRI
+	PhaseDim int // directions in dim[]/pixdim[]
+	SliceDim int // directions in dim[]/pixdim[]
 
-	SliceCode     int32   // code for slice timing pattern
-	SliceStart    int32   // index for start of slices
-	SliceEnd      int32   // index for end of slices
-	SliceDuration float32 // time between individual slices
+	SliceCode     int     // code for slice timing pattern
+	SliceStart    int     // index for start of slices
+	SliceEnd      int     // index for end of slices
+	SliceDuration float64 // time between individual slices
 
 	// Quaternion transform parameters
 	// [when writing a dataset, these are used for qform, NOT qto_xyz]
-	QuaternB, QuaternC, QuaternD, QOffsetX, QOffsetY, QOffsetZ, QFac float32
+	QuaternB, QuaternC, QuaternD, QOffsetX, QOffsetY, QOffsetZ, QFac float64
 
 	QtoXYZ mat44 // qform: transform (i,j,k) to (x,y,z)
 	QtoIJK mat44 // qform: transform (x,y,z) to (i,j,k)
@@ -157,33 +162,33 @@ type Image struct {
 	StoXYZ mat44 // sform: transform (i,j,k) to (x,y,z)
 	StoIJK mat44 // sform: transform (x,y,z) to (i,j,k)
 
-	TOffset float32 // time coordinate offset
+	TOffset float64 // time coordinate offset
 
-	XYZUnits  int32 // dx,dy,dz units: NIFTI_UNITS_* code
-	TimeUnits int32 // dt units: NIFTI_UNITS_* code
+	XYZUnits  int // dx,dy,dz units: NIFTI_UNITS_* code
+	TimeUnits int // dt units: NIFTI_UNITS_* code
 
 	// 0==Analyze, 1==NIFTI-1 (file), 2==NIFTI-1 (2 files), 3==NIFTI-ASCII (1 file)
-	NiftiType int32
+	NiftiType int
 
 	IntentCode int // statistic type (or something)
 
-	IntentP1   float32   // intent parameters
-	IntentP2   float32   // intent parameters
-	IntentP3   float32   // intent parameters
-	IntentName [16]int16 // optional description of intent data
+	IntentP1   float64 // intent parameters
+	IntentP2   float64 // intent parameters
+	IntentP3   float64 // intent parameters
+	IntentName [16]int // optional description of intent data
 
-	Descrip [80]int8 // optional text to describe dataset
-	AuxFile [24]int8 // auxiliary filename
+	Descrip [80]int // optional text to describe dataset
+	AuxFile [24]int // auxiliary filename
 
-	FName       *int8 // header filename
-	IName       *int8 // image filename
-	INameOffset int32 // offset into IName where data start
-	SwapSize    int32 // swap unit in image data (might be 0)
-	ByteOrder   int32 // byte order on disk (MSB_ or LSB_FIRST)
+	FName       *int             // header filename
+	IName       *int             // image filename
+	INameOffset int              // offset into IName where data start
+	SwapSize    int              // swap unit in image data (might be 0)
+	ByteOrder   binary.ByteOrder // byte order on disk (MSB_ or LSB_FIRST)
 
-	data []uint8 // slice of data: nbyper*nvox bytes
+	Data []byte // slice of data: nbyper*nvox bytes
 
-	NumExt int32 // number of extensions in extList
+	NumExt int // number of extensions in extList
 
 	// TODO(kaczmarj): Add extensions list struct
 	// ommitting analyze75_orient
@@ -199,6 +204,8 @@ func check(e error) {
 // Refer to this link for C implementation
 // https://github.com/afni/afni/blob/master/src/nifti/niftilib/nifti1_io.c#L3948-L4042
 func ReadHeader(b []byte) (Header, binary.ByteOrder) {
+
+	log.Debug("Reading header")
 	h := Header{}
 	var order binary.ByteOrder = binary.LittleEndian
 
@@ -219,11 +226,13 @@ func ReadHeader(b []byte) (Header, binary.ByteOrder) {
 
 	validateHeader(h)
 
+	log.Debug("Byte order is", order)
 	return h, order
 }
 
 // Check https://github.com/afni/afni/blob/master/src/nifti/niftilib/nifti1_io.c#L4045-L4104
 func validateHeader(h Header) {
+	log.Debug("Validating header")
 	switch {
 	case h.SizeOfHdr != minHeaderSize:
 		panic("invalid header size for nifti-1")
@@ -236,58 +245,70 @@ func validateHeader(h Header) {
 	}
 }
 
+// ConvertHeaderToImage converts a header to an image.
 // Refer to this on how to create an Image struct.
 // https://github.com/afni/afni/blob/master/src/nifti/niftilib/nifti1_io.c#L5377-L5420
-func convertHeaderToImage(h Header, order binary.ByteOrder) Image {
-	img := Image{
-		NDim: h.Dim[0],
-		Nx:   h.Dim[1],
-		Ny:   h.Dim[2],
-		Nz:   h.Dim[3],
-		Nt:   h.Dim[4],
-		Nu:   h.Dim[5],
-		Nv:   h.Dim[6],
-		Nw:   h.Dim[7],
-		Dim:  h.Dim,
+func ConvertHeaderToImage(h Header, order binary.ByteOrder) *Image {
+
+	log.Debug("Converting header to image")
+
+	img := new(Image)
+
+	img.NDim = int(h.Dim[0])
+	img.Nx = int(h.Dim[1])
+	img.Ny = int(h.Dim[2])
+	img.Nz = int(h.Dim[3])
+	img.Nt = int(h.Dim[4])
+	img.Nu = int(h.Dim[5])
+	img.Nv = int(h.Dim[6])
+	img.Nw = int(h.Dim[7])
+	img.ByteOrder = order
+
+	for i := range img.Dim {
+		img.Dim[i] = int(h.Dim[i])
 	}
 
 	return img
 }
 
-// ReadData reads data.
+// setData sets data into the Image struct. Operates in-place.
 // TODO(kaczmarj): refer to this link for implementation details.
 // https://github.com/afni/afni/blob/master/src/nifti/niftilib/nifti1_io.c#L3712-L3899
 // Total number of bytes in the image is dim[dim[0]] * bitpix / 8
 // This must correspond with the datatype field.
-func ReadData(b []byte, h Header, order binary.ByteOrder) []int16 {
-	var offset int
+func (img *Image) SetData(b []byte, h Header) {
 
+	timeDim := 1
+	if img.Dim[4] > 0 {
+		timeDim = img.Dim[4]
+	}
+
+	statDim := 1
+	if img.Dim[5] > 0 {
+		statDim = img.Dim[5]
+	}
+
+	var offset int
 	if h.VoxOffset < headerSize {
 		offset = headerSize
 	} else {
+		// It should be fine to cast from float32 to int16 here because the index
+		// should always be integer-like.
 		offset = int(h.VoxOffset)
 	}
 
-	// QUESTION: is there a better way to multiply int16 ?
-	dataSize := int(h.Dim[1]) * int(h.Dim[2]) * int(h.Dim[3])
+	dataSize := img.Dim[1] * img.Dim[2] * img.Dim[3] * timeDim * statDim * (int(h.BitPix) / 8)
 
-	data := make([]int16, dataSize)
-	bSlice := b[offset:]
+	log.Debug("")
 
-	fmt.Println("offset", offset)
-	fmt.Println("dataSize", dataSize)
-	fmt.Println("len slice", len(bSlice))
+	img.Data = b[offset : offset+dataSize]
 
-	buf := bytes.NewReader(bSlice)
-	err := binary.Read(buf, order, &data) // TODO: this is still the bottleneck.
-	check(err)
-	return data
 }
 
-func scaleData(data []int16, m float32, b float32) []float32 {
-	dataScaled := make([]float32, len(data))
-	for i, d := range data {
-		dataScaled[i] = m*float32(d) + b
-	}
-	return dataScaled
-}
+// func scaleData(data []int16, m float32, b float32) []float32 {
+// 	dataScaled := make([]float32, len(data))
+// 	for i, d := range data {
+// 		dataScaled[i] = m*float32(d) + b
+// 	}
+// 	return dataScaled
+// }
