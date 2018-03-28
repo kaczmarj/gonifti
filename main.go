cd27d22f92743beb4f1d264588158a77b12e71c2
Jakub Kaczmarzyk <jakubk@mit.edu>
2018-03-28 11:40:29 -0400

    add logging

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/kaczmarj/gonifti/nifti1"
 	"github.com/kaczmarj/gonifti/util"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 
+	log.SetLevel(log.DebugLevel)
+
 	if len(os.Args) < 2 {
 		log.Fatal("nifti filename must be provided")
 	}
-	filepath := os.Args[1]
+	filename := os.Args[1]
 
 	// TODO(kaczmarj): move reading to nifti1.go
-	allBytes := util.ReadBytesParallel(filepath, 8192)
+	allBytes, err := util.ReadBytes(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	header, byteOrder := nifti1.ReadHeader(allBytes)
-	fmt.Println(header)
 
 	image := nifti1.ConvertHeaderToImage(header, byteOrder)
 	image.SetData(allBytes, header)
+
+	log.WithFields(log.Fields{
+		"dataLen": len(image.Data),
+	}).Info("Length of byte data in volume")
 
 }
